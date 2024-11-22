891a0a3c40c7acb08f58c3a4ab4be163d7af2018
polygraphene <polygraphene@gmail.com>
2024-11-22 23:04:28 +0900

    Fix decryption failure on Android 15.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,9 @@ BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
 # Encryption
 BOARD_USES_METADATA_PARTITION := true
 BOARD_USES_QCOM_FBE_DECRYPTION := true
-PLATFORM_VERSION := 14
+# Version number must be larger than system's version, Or FBE decryption should fail.
+# e.g. When PLATFORM_VERSION is 14 and system's android version is 15, decryption will fail because of upgrade required error.
+PLATFORM_VERSION := 99
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 PLATFORM_SECURITY_PATCH := 2099-12-31
 PRODUCT_ENFORCE_VINTF_MANIFEST := true
@@ -112,7 +114,6 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 14
 
 # Extras
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
